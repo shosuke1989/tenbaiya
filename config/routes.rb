29e924d8d1269abdf8171eae3d8218bfab2b1119
shosuke1989 <shosuke.k.1989@googlemail.com>
2021-03-31 16:03:08 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,26 @@
 Rails.application.routes.draw do
   get 'posts/index' => "posts#index"
+  post 'posts/index' => "posts#index"
+  get"posts/new" => "posts#new"
+  
+
+  get "posts/find" => "posts#find"
+  post "posts/findticket" => "posts#findticket"
+
+  get "posts/:id" => "posts#show"
+
+
+  post "posts/:id/sending" => "posts#sending"
+  get "posts/:id/:phonenumber/:buy_id/check" => "posts#check"
+  post "posts/:id/:phonenumber/:ticket_id/input" => "posts#input"
+  get "posts/:id/ticket" => "posts#ticket"
+  post "posts/:id/used" => "posts#used"
+
+ 
+  post "posts/create" => "posts#create"
+
 
   get '/' => "home#top"
   get 'about' => "home#about"
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
