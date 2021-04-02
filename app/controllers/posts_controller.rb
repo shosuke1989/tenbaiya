1682e91ffce695d18e31e076b5cafe20f15d291a
shosuke1989 <shosuke.k.1989@googlemail.com>
2021-04-02 15:33:32 +0900

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -75,7 +75,7 @@ class PostsController < ApplicationController
     #@count_month=Ticket.where('created_at like ?',"%2021-04%")
     
     #@count_month=Ticket.where("created_at < 2021-04-01").count
-    @count_month=Ticket.where('created_at >= ?', Time.zone.parse('2021-04-02')).count
+    #@count_month=Ticket.where('created_at >= ?', Time.zone.parse('2021-04-02')).count
     #@count_month=Ticket.where('created_at >=' Time.zone.parse('2021-04-01')).count
         #@count_month=Ticket.where(id:1).count
 
