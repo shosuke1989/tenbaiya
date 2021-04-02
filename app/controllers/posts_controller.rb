c2432f0edb92de5feb5975bb2e9cd28e53169cee
shosuke1989 <shosuke.k.1989@googlemail.com>
2021-04-02 15:31:37 +0900

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,8 @@ class PostsController < ApplicationController
 
   def show
     @post=Post.find_by(id:params[:id])
+    @post_count=Ticket.where(post_id:params[:id]).count
+
 
   end
 
@@ -70,8 +72,11 @@ class PostsController < ApplicationController
     @count_all=Ticket.where(post_id:@ticket.post_id,phonenumber:@ticket.phonenumber).count
     day=Date.today.to_s[0,7]
     #@count_month=Ticket.where(post_id:@ticket.post_id,phonenumber:@ticket.phonenumber).where('created_at like ?',"%#{day}%").count
-    @count_month=Ticket.where('created_at like ?',"%2021-04%")
-    #@count_month=Ticket.where('created_at >=', Time.zone.parse('2021-04-01')).count
+    #@count_month=Ticket.where('created_at like ?',"%2021-04%")
+    
+    #@count_month=Ticket.where("created_at < 2021-04-01").count
+    @count_month=Ticket.where('created_at >= ?', Time.zone.parse('2021-04-02')).count
+    #@count_month=Ticket.where('created_at >=' Time.zone.parse('2021-04-01')).count
         #@count_month=Ticket.where(id:1).count
 
   end
