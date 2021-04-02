af4d19b8891c2cdd73766826687b7c7746b6d4f6
shosuke1989 <shosuke.k.1989@googlemail.com>
2021-04-02 17:11:08 +0900

    first commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord  
-    validates :content, {presence: true,length: {maximum:140},uniqueness: true}
   
     #belongs_to :buy
   
     has_many :tickets
-  
+    validates :content, {presence: true,length: {maximum:140},uniqueness: true}
+
   
   end
   
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,8 +2,8 @@ class Ticket < ApplicationRecord
        # validates :phonenumber, {presence: true,length: { is: 11 } }
 
    #has_many :posts
+   belongs_to :post
 
    validates :ticket_id, {presence: true, uniqueness: true}
-   belongs_to :post
 
 end
