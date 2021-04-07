add84a16ecb3f489afed5ce49a38be3063b6ed90
shosuke1989 <shosuke.k.1989@googlemail.com>
2021-04-08 00:01:29 +0900

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,15 +49,31 @@ class PostsController < ApplicationController
       redirect_to("/posts/#{@post.id}/#{@preticket.id}/0/check")
       flash[:notice]="SMSを送信しました"
 
+      account_sid = ENV['AC42f6da9063df5b4b9063fec365d89359']
+      auth_token = ENV['97c25bd6c7e6d09699bf86f3c6122855']
+      client = Twilio::REST::Client.new(account_sid, auth_token)
       countly_number="+81#{params[:phonenumber].to_s[1,10]}"
       client = Twilio::REST::Client.new
       client.api.account.messages.create(
-        from: Rails.application.credentials.twilio[:TWILIO_PHONE_NUMBER],
+        from: '+12104054609',
         to: countly_number,
         body: "Tenba×Iya\n
         チケットID:#{@preticket.ticket_id}\n
         確認画面URL:https://tenba-iya.herokuapp.com/posts/#{@post.id}/#{@preticket.id}/#{@preticket.ticket_id}/check"
       )
+
+
+
+      #countly_number="+81#{params[:phonenumber].to_s[1,10]}"
+      #client = Twilio::REST::Client.new
+      #client.api.account.messages.create(
+      #  from: Rails.application.credentials.twilio[:TWILIO_PHONE_NUMBER],
+      #  to: countly_number,
+      #  body: "Tenba×Iya\n
+      #  チケットID:#{@preticket.ticket_id}\n
+      #  確認画面URL:https://tenba-iya.herokuapp.com/posts/#{@post.id}/#{@preticket.id}/#{@preticket.ticket_id}/check"
+      #)
+
       #client.api.account.messages.create(from: Rails.application.credentials.twilio[:TWILIO_PHONE_NUMBER],to: "+818064195239",body: "Hello World!")
 
       #チケットID、電話番号
